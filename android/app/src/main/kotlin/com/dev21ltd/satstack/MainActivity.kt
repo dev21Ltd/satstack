b3b548f61113c0cca2d584ed810b45fcd07cf831
dev21Ltd <dev.21.Ltd@proton.me>
2026-02-18 11:28:41 +0000

    Fix edge-to-edge warnings for Android 15 using WindowCompat

--- android/app/src/main/kotlin/com/dev21ltd/satstack/MainActivity.kt
+++ android/app/src/main/kotlin/com/dev21ltd/satstack/MainActivity.kt
@@ -1,5 +1,13 @@
 package com.dev21ltd.satstack
 
+import android.os.Bundle
+import androidx.core.view.WindowCompat
 import io.flutter.embedding.android.FlutterActivity
 
-class MainActivity : FlutterActivity()
+class MainActivity : FlutterActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        // Tell the system we want to draw behind the status/navigation bars
+        WindowCompat.setDecorFitsSystemWindows(window, false)
+        super.onCreate(savedInstanceState)
+    }
+}
