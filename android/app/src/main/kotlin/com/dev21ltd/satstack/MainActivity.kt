7b8c96b7b080088ccbe9cf245012f535867fa043
dev21Ltd <dev.21.Ltd@proton.me>
2026-09-15 07:25:46 +0100

    Android: drop deprecated edge-to-edge window API

--- android/app/src/main/kotlin/com/dev21ltd/satstack/MainActivity.kt
+++ android/app/src/main/kotlin/com/dev21ltd/satstack/MainActivity.kt
@@ -2,12 +2,10 @@ package com.dev21ltd.satstack
 
 import android.os.Bundle
 import android.view.WindowManager
-import androidx.activity.enableEdgeToEdge
 import io.flutter.embedding.android.FlutterActivity
 
 class MainActivity : FlutterActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
-        enableEdgeToEdge()
         super.onCreate(savedInstanceState)
         window.setFlags(
             WindowManager.LayoutParams.FLAG_SECURE,
